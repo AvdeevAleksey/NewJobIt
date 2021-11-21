de9b25caea5b201343355f919c209d819e362a54
Alexey <avdeev.hexer@yandex.ru>
2021-11-21 21:37:04 +0300

    Start of creation app menu.

--- app/src/main/java/ru/netology/newjobit/view/activity/AppActivity.kt
+++ app/src/main/java/ru/netology/newjobit/view/activity/AppActivity.kt
@@ -1,11 +1,16 @@
 package ru.netology.newjobit.view.activity
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
 import com.google.firebase.messaging.FirebaseMessaging
+import kotlinx.android.synthetic.main.fragment_login.*
 import ru.netology.newjobit.R
 
 class AppActivity : AppCompatActivity(R.layout.activity_app) {
@@ -31,6 +36,25 @@ class AppActivity : AppCompatActivity(R.layout.activity_app) {
 
         FirebaseMessaging.getInstance().token.addOnSuccessListener {
             println(it)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.options_app_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.userSettings -> {
+                Toast.makeText(this, "Choice user settings", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.exit -> {
+                Toast.makeText(this, "Exiting", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> false
         }
     }
 }
